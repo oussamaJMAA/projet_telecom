8912462f28ba903ff8b578b9ddf8191cf71ce6f4
oussama jmaa <jemaaoussama64@gmail.com>
2022-07-31 04:44:25 +0100

    integration_template

---
+++ src/Entity/Course.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CourseRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CourseRepository::class)
+ */
+class Course
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $image;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $nb_enrollments;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $details;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $link;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $nb_likes;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getNbEnrollments(): ?int
+    {
+        return $this->nb_enrollments;
+    }
+
+    public function setNbEnrollments(?int $nb_enrollments): self
+    {
+        $this->nb_enrollments = $nb_enrollments;
+
+        return $this;
+    }
+
+    public function getDetails(): ?string
+    {
+        return $this->details;
+    }
+
+    public function setDetails(string $details): self
+    {
+        $this->details = $details;
+
+        return $this;
+    }
+
+    public function getLink(): ?string
+    {
+        return $this->link;
+    }
+
+    public function setLink(string $link): self
+    {
+        $this->link = $link;
+
+        return $this;
+    }
+
+    public function getNbLikes(): ?int
+    {
+        return $this->nb_likes;
+    }
+
+    public function setNbLikes(int $nb_likes): self
+    {
+        $this->nb_likes = $nb_likes;
+
+        return $this;
+    }
+}
