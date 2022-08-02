62213ca79e204feae6ec93bb73bd35b2b3947f78
khalilDimassi <63399744+khalilDimassi@users.noreply.github.com>
2022-08-02 21:13:47 +0100

    automate questions counting and parsing + bugg fixing

--- scrapping quizz/scrapping.py
+++ scrapping quizz/scrapping.py
@@ -23,21 +23,37 @@ class QuizScrapper:
             return BeautifulSoup(doc, "html.parser")
 
     def parseData(self, fileName):
-        Qs = self.initSoup(fileName).find("div", class_="entry-content").find_all('p', recursive=True, limit=36)
+        Qs = self.initSoup(fileName).find("div", class_="entry-content").find_all('p', limit=36)
         Qs.remove(Qs[0])
         for Q in Qs:
-            Q.find("span").decompose()
+            if Q.find("span") is not None:
+                Q.find("span").decompose()
             for br in Q.find_all("br"):
                 br.decompose()
             splits = Q.text.split('\n')
-            for i in range(len(splits) - 1):
-                self.dataLists[i].append(splits[i])
+            try:
+                print(splits[0])
+                print(splits[4])
+            except (IndexError):
+                print(Q.prettify())
+                break
+            else:
+                for i in range(5):
+                    if ("True" in splits[1] or "True" in splits[2]) and i == 3:
+                        self.dataLists[3].append(None)
+                        self.dataLists[4].append(None)
+                        break
+                    self.dataLists[i].append(splits[i])
 
-        As = self.initSoup(fileName).find_all("div", class_="collapseomatic_content", recursive=True, limit=35)
+        As = self.initSoup(fileName).find_all("div", class_="collapseomatic_content", limit=35)
         for A in As:
             splits = A.text.split('\n')
             self.dataLists[5].append(splits[0])
             self.dataLists[6].append(splits[1])
+
+        for i in range(len(self.dataLists[0]), 35):
+            self.dataLists[5].pop(len(self.dataLists[0]))
+            self.dataLists[6].pop(len(self.dataLists[0]))
 
         Data = {"questions": self.dataLists[0],
                 "A choice": self.dataLists[1],
@@ -47,4 +63,4 @@ class QuizScrapper:
                 "answers": self.dataLists[5],
                 "explanations": self.dataLists[6]}
         df = pd.DataFrame(Data)
-        df.to_json("json files/{}.json".format(fileName), orient='records')
+        df.to_json("json files/{}.json".format(fileName.split('/')[1]), orient='records')
--- scrapping quizz/testfile.py
+++ scrapping quizz/testfile.py
@@ -1,4 +1,4 @@
 from scrapping import QuizScrapper
 
 if __name__ == "__main__":
-    QuizScrapper("html files/1000 Cyber Security MCQ (Multiple Choice Questions) - Sanfoundry_files.html")
+    QuizScrapper("html files/Botnets Questions and Answers - Sanfoundry.html")
