a7b96752a981200a3b6f6bd08cb77df591daaf59
oussama jmaa <jemaaoussama64@gmail.com>
2022-08-05 22:40:11 +0100

    admin_crud

--- src/Controller/CourseController.php
+++ src/Controller/CourseController.php
@@ -7,12 +7,12 @@ use App\Form\CourseType;
 use Symfony\Component\Mime\Email;
 use App\Repository\CourseRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Security\Core\Security;
 /**
  * @Route("/course")
  */
