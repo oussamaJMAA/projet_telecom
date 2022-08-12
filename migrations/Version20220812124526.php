79d99a25601bfd3bc9ba722200fd94319f09da03
oussama jmaa <jemaaoussama64@gmail.com>
2022-08-12 13:46:08 +0100

    fixed_bd_errors

--- migrations/Version20220812124526.php
+++ migrations/Version20220812124526.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220812124041 extends AbstractMigration
+final class Version20220812124526 extends AbstractMigration
 {
     public function getDescription(): string
     {
