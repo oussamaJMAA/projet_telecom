a09c2bf44de7b26dbded903c33f6b906b5bad0fb
oussama jmaa <jemaaoussama64@gmail.com>
2022-08-24 11:39:22 +0100

    courses

---
+++ Scrape_coursera/fix_links.py
@@ -0,0 +1,17 @@
+import pandas as pd
+pd.set_option('display.max_colwidth',None)
+def fix_image_links(x):
+    if x.find('png') != -1:
+        k = x.partition(x[x.find('png') + 3])
+    else:
+        k = x.partition(x[x.find('jpg') + 3])
+    return k[0]
+
+with open('file.json', 'r') as f:
+    df = pd.read_json(f)
+print(df['image'])
+print('****')
+df['image'] = df['image'].apply(lambda x: fix_image_links(x))
+
+print(df['image'])
+df.to_json(r'file2.json', orient='records')
--- Scrape_coursera/json to sql.py
+++ Scrape_coursera/json to sql.py
@@ -3,11 +3,11 @@ import pymysql
 
 conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='', db='projet_telecom')
 cursor = conn.cursor()
-with open('json files/file.json') as f:
+with open('file2.json') as f:
     data = json.load(f)
 for item in data:
-    cursor.execute("INSERT INTO course""(name,image,link,details)""VALUES(%s,%s,%s,%s)", (item["name"], item["image"], item["link"], item["skill"]))
-
+    cursor.execute("INSERT INTO course""(name,image,link,details,rate)""VALUES(%s,%s,%s,%s,%s)",
+                   (item["name"], item["image"], item["link"], item["skill"], item["rate"]))
 
 conn.commit()
 conn.close()
